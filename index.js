85ded652a49c358b71c072438b6fd6928916f463
Jorge Torres <37315291+jorget08@users.noreply.github.com>
2022-06-25 19:28:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,8 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
+
 
 // Syncing all the models at once.
 //conn.sync({ force: true }).then(() => {
