2bfa132c04cfd2303b52ee03a64ce2657b629ad8
Jorge Torres <37315291+jorget08@users.noreply.github.com>
2022-06-25 19:35:53 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -8,7 +8,8 @@ const path = require('path');
 module.exports = () => {
   const connect = () => {
     mongoose.connect(process.env.DB_URL, 
-      {keepAlive: true, useNewUrlParser: true, useUnifiedTopology: true}, 
+      {keepAlive: true, useNewUrlParser: true, useUnifiedTopology: true},
+                     console.log(process.env.DB_URL),
       (err) => {err ? console.log("DB error!") : console.log('Conectado')})
   }
   connect()
